0039459d6bedbcbea28354ae66ded1a680865f48
mochanpypa1709 <65754475+mochanpypa1709@users.noreply.github.com>
2021-05-07 06:49:45 +0700

    Update url_mappings.go

--- app/url_mappings.go
+++ app/url_mappings.go
@@ -4,4 +4,6 @@ import "github.com/mochanpypa1709/bookstore_users-api/controllers"
 
 func MapUrls() {
 	router.GET("/ping", controllers.Ping)
+	router.POST("/users", controllers.GetUser)
+	router.GET("/users/:user_id", controllers.GetUser)
 }
