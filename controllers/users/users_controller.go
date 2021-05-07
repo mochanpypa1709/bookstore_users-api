ff1473fea8ba301beb5411a4dbefdbfe256e2595
mochanpypa1709 <65754475+mochanpypa1709@users.noreply.github.com>
2021-05-07 10:52:13 +0700

    yes

--- controllers/users/users_controller.go
+++ controllers/users/users_controller.go
@@ -6,6 +6,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/mochanpypa1709/bookstore_users-api/domain/users"
 	"github.com/mochanpypa1709/bookstore_users-api/services"
+	"github.com/mochanpypa1709/bookstore_users-api/utils/errors"
 )
 
 func GetUser(c *gin.Context) {
@@ -15,6 +16,12 @@ func GetUser(c *gin.Context) {
 func CreateUser(c *gin.Context) {
 	var user users.User
 	if err := c.ShouldBindJSON(&user); err != nil {
+		restErr := errors.RestErr{
+			Message: "Invalid json body",
+			Status:  http.StatusBadRequest,
+			Error:   "bad_request",
+		}
+		c.JSON(restErr.Status, restErr)
 		return
 	}
 	result, saveErr := services.CreateUser(user)
