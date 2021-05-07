7b834206266c1daa904cdb3db655997a8c19015e
mochanpypa1709 <65754475+mochanpypa1709@users.noreply.github.com>
2021-05-07 10:22:13 +0700

    Update users_controller.go

--- controllers/users/users_controller.go
+++ controllers/users/users_controller.go
@@ -1,9 +1,14 @@
 package users
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/mochanpypa1709/bookstore_users-api/domain/users"
+	"github.com/mochanpypa1709/bookstore_users-api/services"
 )
 
 func GetUser(c *gin.Context) {
@@ -11,5 +16,19 @@ func GetUser(c *gin.Context) {
 }
 
 func CreateUser(c *gin.Context) {
-	c.String(http.StatusNotImplemented, "Implement me!")
+	var user users.User
+	fmt.Println(user)
+	bytes, err := ioutil.ReadAll(c.Request.Body)
+	if err != nil {
+		return
+	}
+	if err := json.Unmarshal(bytes, &user); err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	result, saveErr := services.CreateUser(user)
+	if saveErr != nil {
+		return
+	}
+	c.JSON(http.StatusCreated, result)
 }
